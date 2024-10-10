80f1916bf935bcab28df3447271f48a30144e099
spartanrein <37677855+spartanrein@users.noreply.github.com>
2024-10-10 16:05:33 +0800

    add day of the week

--- app/app.tsx
+++ app/app.tsx
@@ -23,32 +23,27 @@ type ItemProps = {
 };
 
 const Item = ({ item, onPress, backgroundColor, textColor }: ItemProps) => {
-  const formattedDate = new Date(item.title).toLocaleDateString('en-HK', { day: 'numeric' }); // Extract and format the day
-
+    const formattedDate = new Date(item.title).toLocaleDateString('en-HK', { day: 'numeric' }); // Extract and format the day
+    const dayOfWeek = new Date(item.title).toLocaleDateString('en-HK', {weekday: 'short' });
+    
   return (
     <TouchableOpacity onPress={onPress} style={[styles.item, { backgroundColor }]}>
-      <Text style={[styles.title, { color: textColor }]}>{formattedDate}</Text>
+        <Text style={[styles.title, { color: textColor }]}>{dayOfWeek}</Text>
+        <Text style={[styles.title, { color: textColor }]}>{formattedDate}</Text>
     </TouchableOpacity>
   );
 };
 
 const App = () => {
-  const [selectedId, setSelectedId] = useState<string>(DATA[0].id);
-  const [selectedDate, setSelectedDate] = useState<string | null>();
+  const [selectedId, setSelectedId] = useState<string>();
+  const [selectedDate, setSelectedDate] = useState<string | null>(new Date().toLocaleString('en-hk', {
+    day: '2-digit',
+    month: 'short',
+    year: 'numeric',
+  }));
 
   // Find the initial selected item based on selectedId
   const initialSelectedItem = DATA.find((item) => item.id === selectedId);
-
-  // Set the initial selectedDate if an item is found
-  React.useEffect(() => {
-    if (initialSelectedItem) {
-      setSelectedDate(new Date(initialSelectedItem.title).toLocaleDateString('en-HK', {
-        day: '2-digit',
-        month: 'short',
-        year: 'numeric',
-      }));
-    }
-  }, [initialSelectedItem]); // Run only when initialSelectedItem changes
 
   const renderItem = ({ item }: { item: ItemData }) => {
     const backgroundColor = item.id === selectedId ? '#6e3b6e' : '#FFFF';
@@ -59,7 +54,11 @@ const App = () => {
         item={item}
         onPress={() => {
           setSelectedId(item.id);
-          setSelectedDate(new Date(item.title).toLocaleDateString('en-HK'));
+          setSelectedDate(new Date(item.title).toLocaleDateString('en-HK', {
+            day: '2-digit',
+            month: 'short',
+            year: 'numeric',
+          }));
         }}
         backgroundColor={backgroundColor}
         textColor={color}
@@ -92,16 +91,21 @@ const styles = StyleSheet.create({
   item: {
     padding: 10,
     marginVertical: 8,
-    marginHorizontal: 16,
+    marginHorizontal: 8,
+    borderRadius: 24
   },
   title: {
     fontSize: 16,
+    textAlign: 'center'
   },
   header: {
+    display: "flex",
+    justifyContent: 'center',
     backgroundColor: '#f5f5f5',
     padding: 10,
   },
   selectedDate: {
+    textAlign: 'center',
     fontSize: 18,
     fontWeight: 'bold',
   },
@@ -110,59 +114,59 @@ const styles = StyleSheet.create({
 const DATA: ItemData[] = [
     {
         id: "1",
-        title: '2022-10-01 18:00:00.000',
+        title: '2024-10-01 18:00:00.000',
     },
     {
         id: "2",
-        title: '2022-10-02 18:00:00.000',
+        title: '2024-10-02 18:00:00.000',
     },
     {
         id: "3",
-        title: '2022-10-03 18:00:00.000',
+        title: '2024-10-03 18:00:00.000',
     },
     {
         id: "4",
-        title: '2022-10-04 18:00:00.000',
+        title: '2024-10-04 18:00:00.000',
     },
     {
         id: "5",
-        title: '2022-10-05 18:00:00.000',
+        title: '2024-10-05 18:00:00.000',
     },
     {
         id: "6",
-        title: '2022-10-06 18:00:00.000',
+        title: '2024-10-06 18:00:00.000',
     },
     {
         id: "7",
-        title: '2022-10-07 18:00:00.000',
+        title: '2024-10-07 18:00:00.000',
     },
     {
         id: "8",
-        title: '2022-10-08 18:00:00.000',
+        title: '2024-10-08 18:00:00.000',
     },
     {
         id: "9",
-        title: '2022-10-10 18:00:00.000',
+        title: '2024-10-09 18:00:00.000',
     },
     {
         id: "10",
-        title: '2022-10-10 18:00:00.000',
+        title: '2024-10-10 18:00:00.000',
     },
     {
         id: "11",
-        title: '2022-10-11 18:00:00.000',
+        title: '2024-10-11 18:00:00.000',
     },
     {
         id: "12",
-        title: '2022-10-12 18:00:00.000',
+        title: '2024-10-12 18:00:00.000',
     },
     {
         id: "13",
-        title: '2022-10-13 18:00:00.000',
+        title: '2024-10-13 18:00:00.000',
     },
     {
         id: "14",
-        title: '2022-10-14 18:00:00.000',
+        title: '2024-10-14 18:00:00.000',
     },
 ];
 
